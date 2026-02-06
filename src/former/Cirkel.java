38ed8350f8db5193609ae307552a157568926992
okama01 <okama01@HUGI0255S.adm.huddinge.se>
2026-02-06 09:32:37 +0100

    start av projekt

--- src/former/Cirkel.java
+++ src/former/Cirkel.java
@@ -30,7 +30,7 @@ public class Cirkel extends Form {
 
         this.radie = r;
         this.fylld = f;
-        color = new Color((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
+        Color color = new Color((int) (Math.random() * 255), (int) (Math.random() * 255), (int) (Math.random() * 255));
     }
 
     @Override
